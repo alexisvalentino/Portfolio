4f1d97adc8b88c9c3d9235baa57792e8e04a7e0b
alexisvalentino <alexis01valentino@gmail.com>
2023-07-11 02:08:40 +0800

    Rollback of Blog

--- components/blog/BlogItem.js
+++ components/blog/BlogItem.js
@@ -1,7 +1,7 @@
 import Image from "next/image";
 export default function BlogItem({ post }) {
   const redirectToHashnode = () => {
-    window.open("https://medium.com/@alexisvalentino/" + post.slug, "_blank");
+    window.open("https://blog.danielcranney.com/" + post.slug, "_blank");
   };
 
   const getDateAdded = () => {
